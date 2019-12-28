a658ba6de73c716312ce679d685655fbceb77700
gtargino <gustavodiniztargino@hotmail.com>
2019-12-28 02:45:24 -0300

    chore: Send email to provider via nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: 'smtp.mailtrap.io',
+    port: '2525',
+    secure: false,
+    auth: {
+        user: '66f787770c92b3',
+        pass: '364735e19a4e36',
+    },
+    default: {
+        from: 'gobarber team <noreply@gobarber.com>',
+    },
+};
